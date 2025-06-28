84627e01957ed03909622a8ab94e77154ede4b4a
eternaltribute <brittsolarteq@gmail.com>
2025-06-28 10:57:36 -0400

    testing

---
+++ closet.py
@@ -0,0 +1,64 @@
+closet = [
+    # Plain White Target Crop top
+    {
+        "name": "White Crop Top",
+        "article": "tops",
+        "styleTags": ["neutral", "basic", "comfy"],
+        "seasonTags": ["spring", "summer", "fall"],
+        "indoorOnly": False,
+        "minTemp": 55,
+        "maxTemp": 100,
+        "color": "white",
+        "pattern": ["solid", "plain"],
+        "material": "cotton"
+    },
+
+    # Doc Martens Oxford
+    {
+        "name": "Docs",
+        "article": "shoes",
+        "styleTags": ["neutral", "edgy", "grunge"],
+        "seasonTags": ["spring", "summer", "fall", "winter"],
+        "weatherTags": ["sunny", "rainy", "windy", "cloudy", "snowing", "freezing"],
+        "indoorOnly": False,
+        "minTemp": 20,
+        "maxTemp": 100,
+        "color": "black",
+        "pattern": ["solid", "plain"],
+        "material": "rubber"
+    },
+
+    # Abercrombie Medium Wash Solid Jeans
+    {
+        "name": "High Waisted Medium Wash Jeans",
+        "article": "bottoms",
+        "styleTags": ["neutral", "basic", "comfy", "casual"],
+        "seasonTags": ["spring", "summer", "fall"],
+        "weatherTags": ["sunny", "rainy", "windy", "cloudy", "snowing", "freezing"],
+        "indoorOnly": False,
+        "minTemp": 20,
+        "maxTemp": 90,
+        "color": "blue",
+        "pattern": ["solid", "plain"],
+        "material": "denim"
+    }
+        #TEMPLATE
+    {
+        "name": "",
+        "article": "",           # e.g., "top", "bottom", "outerwear", "shoes"
+        "pattern": "",           # e.g., "floral", "solid", "striped"
+        "color": "",
+        "styleTags": [],         # e.g., ["boho", "casual"]
+        "seasonTags": [],        # e.g., ["spring", "summer"]
+        "weatherTags": [],       # e.g., ["rainy", "windy"]
+        "indoorOnly": False,
+        "minTemp": 0,
+        "maxTemp": 100
+    }
+]
+
+
+#testing
+
+for item in closet:
+    print(f"{item['name']} - {item['article']} - {item['material']} ({item['color']})")
